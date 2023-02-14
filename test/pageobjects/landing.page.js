1191c8823b46945b18e1b7d05d84e94400523971
Frankie47 <102690683+Frankie47@users.noreply.github.com>
2023-02-14 21:35:32 +0000

    code clean up

--- test/pageobjects/landing.page.js
+++ test/pageobjects/landing.page.js
@@ -9,17 +9,7 @@ class LandingPage extends Page {
     /**
      * define selectors using getter methods
      */
-    get inputUsername () {
-        return $('#username');
-    }
-
-    get inputPassword () {
-        return $('#password');
-    }
-
-    get btnSubmit () {
-        return $('button[type="submit"]');
-    }
+  
 
     get servicesBtn(){
         return $("//span[@title='Services']")
@@ -88,15 +78,7 @@ class LandingPage extends Page {
         return $("button.chakra-button.ii-7kliwd")
     
     }
-    /**
-     * a method to encapsule automation code to interact with the page
-     * e.g. to login using username and password
-     */
-    async login (username, password) {
-        await this.inputUsername.setValue(username);
-        await this.inputPassword.setValue(password);
-        await this.btnSubmit.click();
-    }
+
 
     async navigateToTradingAccount(){
         await this.acceptCookiesBtn.click()
